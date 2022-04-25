5ec8c22704d9f17ad10da4b1618e788499d3a4a4
Vida Feng <vidaffeng@gmail.com>
2022-04-25 12:44:15 -0400

    removed nodemon

--- client/components/Confirmation.js
+++ client/components/Confirmation.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import { Button, ButtonGroup } from "@chakra-ui/react";
 
-const Confirmation = () => {
-  return (
-    <div>
-      <h1>Thank You for Your Order!</h1>
-      <br />
-      <Link to="/home">
-        <Button colorScheme="teal">Home</Button>
-      </Link>
-    </div>
-  );
-};
+// const Confirmation = () => {
+//   return (
+//     <div>
+//       <h1>Thank You for Your Order!</h1>
+//       <br />
+//       <Link to="/home">
+//         <Button colorScheme="teal">Home</Button>
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default Confirmation;
+// export default Confirmation;
